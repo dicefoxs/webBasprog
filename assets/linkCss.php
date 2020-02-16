023a8183b46b2e1f1fc58ac8b2bef6078dcf34e8
dicefoxs <44152008+dicefoxs@users.noreply.github.com>
2020-02-17 05:45:04 +0700

    third
    3rd upload

---
+++ assets/linkCss.php
@@ -0,0 +1,7 @@
+<!-- Bootstrap CSS & JS -->
+<title>Laboratorium Bahasa Pemrograman | ITATS</title>
+<link rel="icon" type="image/gif/png" href="../assets/img/logo.jpg">
+<link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.min.css">
+<!-- <link rel="stylesheet" type="text/css" href="../assets/css/bootstrapValidator.css"> -->
+<link rel="stylesheet" type="text/css" href="../client/style.css">
+<link rel="stylesheet" href="../assets/js/">
